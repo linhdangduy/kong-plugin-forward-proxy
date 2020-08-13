4fdfb74c8f151d5dc01bfdeab4dd248658e470d6
linhdangduy <dangduylinh.bk@gmail.com>
2020-08-13 20:14:48 +0900

    use kong interface

--- kong/plugins/forward-proxy/handler.lua
+++ kong/plugins/forward-proxy/handler.lua
@@ -9,11 +9,17 @@ end
 
 function plugin:access(plugin_conf)
     plugin.super.access(self)
+    ngx.log(ngx.NOTICE, string.format("kong.version: %s", kong.version))
     ngx.log(ngx.NOTICE, string.format("before ngx.var.upstream_uri: %s", ngx.var.upstream_uri))
     ngx.log(ngx.NOTICE, string.format("before ngx.var.upstream_host: %s", ngx.var.upstream_host))
     ngx.log(ngx.NOTICE, string.format("before ngx.var.upstream_port: %s", ngx.var.upstream_port))
-    ngx.var.upstream_host = ngx.ctx.service.host -- string.format("%s:%s", ngx.ctx.service.host, ngx.ctx.service.port)
-    ngx.log(ngx.NOTICE, string.format("ngx.var.upstream_host %s", ngx.var.upstream_host))
+    -- ngx.var.upstream_host = ngx.ctx.service.host -- string.format("%s:%s", ngx.ctx.service.host, ngx.ctx.service.port)
+    -- ngx.log(ngx.NOTICE, string.format("ngx.var.upstream_host %s", ngx.var.upstream_host))
+    local ok, err = kong.service.set_upstream(ngx.ctx.service.host)
+    if not ok then
+        kong.log.err(err)
+        return
+    end
     ngx.log(ngx.NOTICE, string.format("plugin_conf.proxy_host:plugin_conf.proxy_port %s:%s", plugin_conf.proxy_host, plugin_conf.proxy_port))
     ngx.ctx.balancer_data.host = plugin_conf.proxy_host
     ngx.ctx.balancer_data.port = plugin_conf.proxy_port
