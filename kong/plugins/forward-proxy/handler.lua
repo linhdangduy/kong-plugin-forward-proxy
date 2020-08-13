2953faba58d0d63524d4ac38a72907e912cda685
linhdangduy <dangduylinh.bk@gmail.com>
2020-08-14 01:25:59 +0900

    delete kong sdk

--- kong/plugins/forward-proxy/handler.lua
+++ kong/plugins/forward-proxy/handler.lua
@@ -13,13 +13,8 @@ function plugin:access(plugin_conf)
     ngx.log(ngx.NOTICE, string.format("before ngx.var.upstream_uri: %s", ngx.var.upstream_uri))
     ngx.log(ngx.NOTICE, string.format("before ngx.var.upstream_host: %s", ngx.var.upstream_host))
     ngx.log(ngx.NOTICE, string.format("before ngx.var.upstream_port: %s", ngx.var.upstream_port))
-    -- ngx.var.upstream_host = ngx.ctx.service.host -- string.format("%s:%s", ngx.ctx.service.host, ngx.ctx.service.port)
-    -- ngx.log(ngx.NOTICE, string.format("ngx.var.upstream_host %s", ngx.var.upstream_host))
-    local ok, err = kong.service.set_upstream(ngx.ctx.service.host)
-    if not ok then
-        kong.log.err(err)
-        return
-    end
+    ngx.var.upstream_host = ngx.ctx.service.host -- string.format("%s:%s", ngx.ctx.service.host, ngx.ctx.service.port)
+    ngx.log(ngx.NOTICE, string.format("ngx.var.upstream_host %s", ngx.var.upstream_host))
     ngx.log(ngx.NOTICE, string.format("plugin_conf.proxy_host:plugin_conf.proxy_port %s:%s", plugin_conf.proxy_host, plugin_conf.proxy_port))
     ngx.ctx.balancer_data.host = plugin_conf.proxy_host
     ngx.ctx.balancer_data.port = plugin_conf.proxy_port
